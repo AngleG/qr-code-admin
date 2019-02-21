2118360b1ffd73df58b543bc3e914e74b9e20c59
paul wang <wangj@abc-group.cn>
2019-02-21 18:37:18 +0800

    添加全局vue方法

---
+++ src/lib/plugins/vue-global.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import { Message } from 'element-ui'
+/**
+ * 自定义Vue全局toast方法 element message
+ * @param message 用户自定义内容
+ * @param type 可选：success/warning/info/error，默认warning
+ */
+Vue.prototype.$toast = function (message, type = 'warning') {
+    Message({
+      message,
+      type,
+      customClass: 'custom-toast'
+    })
+};
--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 //按需导入element-ui 组件
-import './lib/plugins/element-import'
+import './lib/plugins/element'
 //引入自定义初始化css
 import './static/css/index.css'
-
+//引入vue全局自定义方法
+import './lib/plugins/vue-global'
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
