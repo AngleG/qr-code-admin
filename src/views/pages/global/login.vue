2e55294014152e4a7e0d06b04f3494209efde956
wxx <1464661791@qq.com>
2019-02-23 15:54:50 +0800

    登录页控制表单输入字符最大长度； 校验修复

--- src/views/pages/global/login.vue
+++ src/views/pages/global/login.vue
@@ -2,26 +2,27 @@
   <div class="login-container">
     <div class="login-content">
       <p class="form-label">
-        <el-input size="medium" prefix-icon="iconfont icon-yonghu" placeholder="请输入账号" v-model="username"/>
+        <el-input size="medium" prefix-icon="iconfont icon-yonghu" placeholder="请输入账号" v-model="username" maxlength="20"/>
       </p>
       <p class="form-label">
-        <el-input size="medium" prefix-icon="iconfont icon-mima" type="password" placeholder="请输入密码" v-model="password"/>
+        <el-input size="medium" prefix-icon="iconfont icon-mima" type="password" placeholder="请输入密码" v-model="password" maxlength="20"/>
       </p>
       <el-button class="confirm" @click="login" type="primary" round>登录</el-button>
     </div>
     <!--<el-row >-->
-      <!--<el-col :span="5" :offset="1" :xs="{span: 24, offset: 0}" :sm="{span: 7, offset: 1}" :lg="{span: 5, offset: 1}" :xl="{span: 4, offset: 0.8}"><div class="grid-content bg-purple"></div></el-col>-->
-      <!--<el-col :span="5" :offset="1" :xs="{span: 24, offset: 0}" :sm="{span: 7, offset: 1}" :lg="{span: 5, offset: 1}" :xl="{span: 4, offset: 0.8}"><div class="grid-content bg-purple-light"></div></el-col>-->
-      <!--<el-col :span="5" :offset="1" :xs="{span: 24, offset: 0}" :sm="{span: 7, offset: 1}" :lg="{span: 5, offset: 1}" :xl="{span: 4, offset: 0.8}"><div class="grid-content bg-purple"></div></el-col>-->
-      <!--<el-col :span="5" :offset="1" :xs="{span: 24, offset: 0}" :sm="{span: 7, offset: 1}" :lg="{span: 5, offset: 1}" :xl="{span: 4, offset: 0.8}"><div class="grid-content" style="background-color: red"></div></el-col>-->
-      <!--<el-col :span="5" :offset="1" :xs="{span: 24, offset: 0}" :sm="{span: 7, offset: 1}" :lg="{span: 5, offset: 1}" :xl="{span: 4, offset: 0.8}"><div class="grid-content" style="background-color: yellow"></div></el-col>-->
+    <!--<el-col :span="5" :offset="1" :xs="{span: 24, offset: 0}" :sm="{span: 7, offset: 1}" :lg="{span: 5, offset: 1}" :xl="{span: 4, offset: 0.8}"><div class="grid-content bg-purple"></div></el-col>-->
+    <!--<el-col :span="5" :offset="1" :xs="{span: 24, offset: 0}" :sm="{span: 7, offset: 1}" :lg="{span: 5, offset: 1}" :xl="{span: 4, offset: 0.8}"><div class="grid-content bg-purple-light"></div></el-col>-->
+    <!--<el-col :span="5" :offset="1" :xs="{span: 24, offset: 0}" :sm="{span: 7, offset: 1}" :lg="{span: 5, offset: 1}" :xl="{span: 4, offset: 0.8}"><div class="grid-content bg-purple"></div></el-col>-->
+    <!--<el-col :span="5" :offset="1" :xs="{span: 24, offset: 0}" :sm="{span: 7, offset: 1}" :lg="{span: 5, offset: 1}" :xl="{span: 4, offset: 0.8}"><div class="grid-content" style="background-color: red"></div></el-col>-->
+    <!--<el-col :span="5" :offset="1" :xs="{span: 24, offset: 0}" :sm="{span: 7, offset: 1}" :lg="{span: 5, offset: 1}" :xl="{span: 4, offset: 0.8}"><div class="grid-content" style="background-color: yellow"></div></el-col>-->
     <!--</el-row>-->
   </div>
 </template>
 
 <script>
   import webApi from '../../../lib/api'
-	export default {
+
+  export default {
     data() {
       return {
         username: 'testadmin',
@@ -34,7 +35,7 @@
         if (!params.user) {
           return this.$toast('请输入账号')
         }
-        if (!params.user) {
+        if (!params.pass) {
           return this.$toast('请输入密码')
         }
         let res = await webApi.login({user: this.username, pass: this.password});
@@ -48,36 +49,41 @@
         }
       }
     },
-	}
+  }
 </script>
 
 <style lang="scss" scoped>
-  .login-container{
+  .login-container {
     display: flex;
     justify-content: center;
     align-items: center;
     width: 100%;
     height: 100%;
     background: url("../../../static/assets/login-bg.jpg") center center no-repeat;
-    .login-content{
+
+    .login-content {
       box-sizing: border-box;
       padding-top: 150px;
       width: 658px;
       height: 466px;
       background: url("../../../static/images/login-form.png") center center no-repeat;
       text-align: center;
-      .form-label{
+
+      .form-label {
         margin-top: 25px;
-        .iconfont{
+
+        .iconfont {
           font-size: 14px;
           color: #fff;
         }
-        /deep/ .el-input{
+
+        /deep/ .el-input {
           display: inline-block;
           width: 400px;
         }
       }
-      .confirm{
+
+      .confirm {
         margin-top: 40px;
         padding: 9px 50px;
       }
@@ -86,26 +92,33 @@
 
   .el-row {
     margin-bottom: 20px;
+
     &:last-child {
       margin-bottom: 0;
     }
   }
+
   .el-col {
     border-radius: 4px;
   }
+
   .bg-purple-dark {
     background: #99a9bf;
   }
+
   .bg-purple {
     background: #d3dce6;
   }
+
   .bg-purple-light {
     background: #e5e9f2;
   }
+
   .grid-content {
     border-radius: 4px;
     min-height: 236px;
   }
+
   .row-bg {
     padding: 10px 0;
     background-color: #f9fafc;
