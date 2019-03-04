bcedab8b9744b921954aab3e250519ddaac35f3d
paul wang <wangj@abc-group.cn>
2019-03-04 16:25:51 +0800

    兑换订单页面对接完成

--- src/views/pages/consumer/exchange-order/index.vue
+++ src/views/pages/consumer/exchange-order/index.vue
@@ -30,6 +30,42 @@
       <element-table v-loading="isLoading" :table-columns="tableColumns" :table-data="tableData"></element-table>
       <customize-pagination :total="total" :currentPage="totalpages"/>
     </div>
+    <el-dialog
+      title="兑换单详情"
+      :visible.sync="dialogVisible"
+      :close-on-click-modal="false"
+      @close="closeDialog"
+      width="600px">
+      <div class="exchange-dialog-content" v-if="exchangeDetail">
+        <div class="clearfix">
+          <base-item class="w_50" inline><template slot="label">来源礼券ID:</template>{{ exchangeDetail.fromcouponid }}</base-item>
+          <base-item class="w_50" inline><template slot="label">目标礼券ID:</template>{{ exchangeDetail.tocouponid }}</base-item>
+          <base-item class="w_50" inline><template slot="label">下单时间:</template>{{ exchangeDetail.createtime }}</base-item>
+          <base-item class="w_50" inline><template slot="label">数量:</template>{{ exchangeDetail.codenum }}</base-item>
+        </div>
+        <base-item><template slot="label">收货地址:</template>{{ exchangeDetail.recprov }}{{exchangeDetail.recity}}{{exchangeDetail.recounty}}{{exchangeDetail.recstreet}}</base-item>
+        <div class="clearfix">
+          <base-item class="w_50" inline><template slot="label">收货人名称:</template>{{ exchangeDetail.recontact }}</base-item>
+          <base-item class="w_50" inline><template slot="label">收货人电话:</template>{{ exchangeDetail.recphone }}</base-item>
+        </div>
+        <base-item><template slot="label">发货地址:</template>{{ exchangeDetail.sendprov }}{{exchangeDetail.sendcity}}{{exchangeDetail.sendcounty}}{{exchangeDetail.sendstreet}}</base-item>
+        <div class="clearfix">
+          <base-item class="w_50" inline><template slot="label">兑换人手机:</template>{{ exchangeDetail.usermobile }}</base-item>
+          <base-item class="w_50" inline><template slot="label">祝福语:</template>{{ exchangeDetail.hello }}</base-item>
+          <base-item class="w_50" inline><template slot="label">发货人名称:</template>{{ exchangeDetail.sendcontact }}</base-item>
+          <base-item class="w_50" inline><template slot="label">发货人电话:</template>{{ exchangeDetail.sendphone }}</base-item>
+          <base-item class="w_50" inline><template slot="label">来源:</template>{{ exchangeDetail.from }}</base-item>
+          <base-item class="w_50" inline><template slot="label">昵称:</template>{{ exchangeDetail.usernick }}</base-item>
+          <base-item class="w_50" inline><template slot="label">快递公司:</template><template v-if="exchangeDetail.delcom">{{ exchangeDetail.delcom }}</template></base-item>
+          <base-item class="w_50" inline><template slot="label">快递单号:</template><template v-if="exchangeDetail.delid">{{ exchangeDetail.delid }}</template></base-item>
+          <base-item class="w_50" inline><template slot="label">性别:</template>{{ exchangeDetail.usergender }}</base-item>
+          <base-item class="w_50" inline><template slot="label">头像:</template><template v-if="exchangeDetail.userhead"><span class="user-head"><img :src="exchangeDetail.userhead" width="100%" height="100%"></span></template></base-item>
+        </div>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button size="small" @click="closeDialog" round>关闭</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -52,12 +88,14 @@
           {title: '兑换来源', align: 'center', key: 'fromcouponname' },
           {title: '兑换目标', align: 'center', key: 'tocouponname' },
           {title: '更新时间', align: 'center', key: 'lastupdatime' },
-          {title: '操作', align: 'center', render: (h, params) => <el-button size="medium" type="text">查看</el-button>},
+          {title: '操作', align: 'center', render: (h, params) => <el-button onClick={this.setCurrentExchangeDetail.bind(this, params.row)} size="medium" type="text">查看</el-button>},
         ],
         tableData: [],
         isLoading: false,
         total: 0,
-        totalpages: 0
+        totalpages: 0,
+        dialogVisible: false,
+        exchangeDetail: null
       }
     },
     created() {
@@ -111,12 +149,44 @@
         }else {
           this.$toast(res.message, 'error');
         }
+      },
+      openDialog() {
+        this.dialogVisible = true;
+      },
+      closeDialog() {
+        this.dialogVisible = false;
+        if (this.exchangeDetail) {
+          this.$restObjectAttribute(this.exchangeDetail)
+        }
+      },
+      setCurrentExchangeDetail(row) {
+        if (row) {
+          this.exchangeDetail = this.$_.cloneDeep(row);
+          this.openDialog();
+        }
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
+  .exchange-dialog-content{
+    text-align: left;
+    /deep/ {
+      @include customFormItem(85px, 40px, #afafaf, #fff, false);
+      .w_50{
+        width: 50%;
+        float: left;
+      }
+      .user-head{
+        display: inline-block;
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        overflow: hidden;
+      }
+    }
+  }
   .exchange-order {
     .exchange-order_search {
       min-height: 50px;
