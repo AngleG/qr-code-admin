2205e070c4e464577656c1861e5b4aae74c71d32
paul wang <wangj@abc-group.cn>
2019-03-04 16:25:34 +0800

    添加全局base-item组件

--- src/lib/plugins/vue-components.js
+++ src/lib/plugins/vue-components.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import elementTable from '../../views/common/table'
 import customizePagination from '../../views/common/customize-pagination'
-
+import baseItem from "../../views/base/base-item";
 //注册全局组件
 Vue.component('element-table', elementTable);
 Vue.component('customize-pagination', customizePagination);
+Vue.component('base-item', baseItem);
---
+++ src/views/base/base-item.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :class="['custom-form-item', position === 'top' ?  'label-position__top' : '']" :style="{width, display: inline ? 'inline-block' : 'block'}">
+    <div class="custom-form-item__el custom-form-item__label" :style="{width: typeof labelWidth === 'string' ? labelWidth : labelWidth + 'px'}"><slot name="label"></slot></div>
+    <div class="custom-form-item__el custom-form-item__content" :style="{marginLeft: typeof labelWidth === 'string' ? labelWidth : labelWidth + 'px', color: contentColor}"><slot></slot></div>
+  </div>
+</template>
+
+<script>
+	export default {
+	  props: {
+	    width: String,
+      labelWidth: [String, Number],
+      contentColor: String,
+      inline: {
+	      type: Boolean,
+        default: false
+      },
+      position: {
+	      type: String,
+        default: 'left'
+      }
+    }
+	}
+</script>
+
+<style lang="scss" scoped>
+  .custom-form-item{
+    &.label-position__top{
+      .custom-form-item__el.custom-form-item__label{
+        float: none;
+        display: inline-block;
+        text-align: left;
+      }
+      .custom-form-item__el.custom-form-item__content{
+        margin-left: 0;
+      }
+    }
+  }
+
+</style>
