2bc71ea0561f8a881603cee531426aa3ea9cb24b
paul wang <wangj@abc-group.cn>
2019-05-22 18:15:10 +0800

    通用table组件添加ref和selectable方法

--- src/views/common/table.vue
+++ src/views/common/table.vue
@@ -8,6 +8,7 @@
       :max-height="600"
       fit
       :style="{'width': width}"
+      ref="customize-table"
       v-bind="$attrs"
       v-on="$listeners">
         <template v-for="column in tableColumns">
@@ -16,10 +17,11 @@
           :width="column.width || null"
           :prop="column.key"
           :label="column.title"
-          :align="column.align || center"
+          :align="column.align || 'center'"
           :fixed="column.fixed || null"
           :sortable="column.sortable || null"
           :formatter="column.formatter || null"
+          :selectable="column.selectable || null"
           :show-overflow-tooltip="column.isTooltip !== undefined ? column.isTooltip : true"
           :key="column.title"
           :render-header="column.renderHeader || null"
@@ -30,10 +32,11 @@
             :type="column.type || null"
             :width="column.width || null"
             :label="column.title"
-            :align="column.align"
+            :align="column.align || 'center'"
             :fixed="column.fixed || null"
             :sortable="column.sortable || null"
             :formatter="column.formatter || null"
+            :selectable="column.selectable || null"
             :show-overflow-tooltip="column.isTooltip || true"
             :key="column.title"
             :render-header="column.renderHeader || null">
